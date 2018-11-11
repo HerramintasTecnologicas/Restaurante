1b4afd38cab5a0b95d2538e8c4f677d1f9b566ba
Athroscf <fialloschris1@gmail.com>
2018-11-11 17:39:38 -0600

    #2 Continuacion del primer commit.

---
+++ Restaurante/frmPagosCaja.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Restaurante
+{
+    partial class frmPagosCaja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPagosCaja));
+            this.lbTipoPagoCaja = new System.Windows.Forms.ListBox();
+            this.lblTipoPagoCaja = new System.Windows.Forms.Label();
+            this.lblMontoPagosCaja = new System.Windows.Forms.Label();
+            this.txtMontoPagosCaja = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dtpFechaPagosCaja = new System.Windows.Forms.DateTimePicker();
+            this.lblFechaPagosCaja = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnSalirAperturarCaja = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlTitulo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTipoPagoCaja
+            // 
+            this.lbTipoPagoCaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTipoPagoCaja.FormattingEnabled = true;
+            this.lbTipoPagoCaja.ItemHeight = 20;
+            this.lbTipoPagoCaja.Location = new System.Drawing.Point(146, 88);
+            this.lbTipoPagoCaja.Name = "lbTipoPagoCaja";
+            this.lbTipoPagoCaja.Size = new System.Drawing.Size(164, 24);
+            this.lbTipoPagoCaja.TabIndex = 1;
+            // 
+            // lblTipoPagoCaja
+            // 
+            this.lblTipoPagoCaja.AutoSize = true;
+            this.lblTipoPagoCaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoPagoCaja.Location = new System.Drawing.Point(37, 92);
+            this.lblTipoPagoCaja.Name = "lblTipoPagoCaja";
+            this.lblTipoPagoCaja.Size = new System.Drawing.Size(39, 20);
+            this.lblTipoPagoCaja.TabIndex = 2;
+            this.lblTipoPagoCaja.Text = "Tipo";
+            // 
+            // lblMontoPagosCaja
+            // 
+            this.lblMontoPagosCaja.AutoSize = true;
+            this.lblMontoPagosCaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMontoPagosCaja.Location = new System.Drawing.Point(37, 128);
+            this.lblMontoPagosCaja.Name = "lblMontoPagosCaja";
+            this.lblMontoPagosCaja.Size = new System.Drawing.Size(54, 20);
+            this.lblMontoPagosCaja.TabIndex = 3;
+            this.lblMontoPagosCaja.Text = "Monto";
+            // 
+            // txtMontoPagosCaja
+            // 
+            this.txtMontoPagosCaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMontoPagosCaja.Location = new System.Drawing.Point(146, 128);
+            this.txtMontoPagosCaja.Name = "txtMontoPagosCaja";
+            this.txtMontoPagosCaja.Size = new System.Drawing.Size(164, 26);
+            this.txtMontoPagosCaja.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Descripción";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(146, 212);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 122);
+            this.textBox1.TabIndex = 6;
+            // 
+            // dtpFechaPagosCaja
+            // 
+            this.dtpFechaPagosCaja.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFechaPagosCaja.Location = new System.Drawing.Point(146, 168);
+            this.dtpFechaPagosCaja.Name = "dtpFechaPagosCaja";
+            this.dtpFechaPagosCaja.Size = new System.Drawing.Size(193, 20);
+            this.dtpFechaPagosCaja.TabIndex = 17;
+            // 
+            // lblFechaPagosCaja
+            // 
+            this.lblFechaPagosCaja.AutoSize = true;
+            this.lblFechaPagosCaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaPagosCaja.Location = new System.Drawing.Point(37, 169);
+            this.lblFechaPagosCaja.Name = "lblFechaPagosCaja";
+            this.lblFechaPagosCaja.Size = new System.Drawing.Size(54, 20);
+            this.lblFechaPagosCaja.TabIndex = 16;
+            this.lblFechaPagosCaja.Text = "Fecha";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 356);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(570, 79);
+            this.pictureBox1.TabIndex = 49;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlTitulo
+            // 
+            this.pnlTitulo.Controls.Add(this.label2);
+            this.pnlTitulo.Location = new System.Drawing.Point(102, 12);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(341, 62);
+            this.pnlTitulo.TabIndex = 50;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(125)))));
+            this.label2.Location = new System.Drawing.Point(3, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(335, 30);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Pago de Servicios Publicos";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSalir.BackgroundImage")));
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Location = new System.Drawing.Point(525, -1);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(29, 29);
+            this.btnSalir.TabIndex = 51;
+            this.btnSalir.TabStop = false;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnSalirAperturarCaja
+            // 
+            this.btnSalirAperturarCaja.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(76)))), ((int)(((byte)(126)))));
+            this.btnSalirAperturarCaja.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalirAperturarCaja.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnSalirAperturarCaja.Location = new System.Drawing.Point(424, 147);
+            this.btnSalirAperturarCaja.Name = "btnSalirAperturarCaja";
+            this.btnSalirAperturarCaja.Size = new System.Drawing.Size(75, 25);
+            this.btnSalirAperturarCaja.TabIndex = 52;
+            this.btnSalirAperturarCaja.Text = "Salir";
+            this.btnSalirAperturarCaja.UseVisualStyleBackColor = false;
+            this.btnSalirAperturarCaja.Click += new System.EventHandler(this.btnSalirAperturarCaja_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(76)))), ((int)(((byte)(126)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(424, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 53;
+            this.button1.Text = "Registrar Pago";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // frmPagosCaja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 434);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSalirAperturarCaja);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.pnlTitulo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dtpFechaPagosCaja);
+            this.Controls.Add(this.lblFechaPagosCaja);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMontoPagosCaja);
+            this.Controls.Add(this.lblMontoPagosCaja);
+            this.Controls.Add(this.lblTipoPagoCaja);
+            this.Controls.Add(this.lbTipoPagoCaja);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmPagosCaja";
+            this.Text = "Pagos";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlTitulo.ResumeLayout(false);
+            this.pnlTitulo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lbTipoPagoCaja;
+        private System.Windows.Forms.Label lblTipoPagoCaja;
+        private System.Windows.Forms.Label lblMontoPagosCaja;
+        private System.Windows.Forms.TextBox txtMontoPagosCaja;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dtpFechaPagosCaja;
+        private System.Windows.Forms.Label lblFechaPagosCaja;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnSalirAperturarCaja;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Restaurante/frmTransaccionesCaja.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurante
+{
+    public partial class frmTransaccionesCaja : Form
+    {
+        public frmTransaccionesCaja()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirCaja_Click(object sender, EventArgs e)
+        {
+            frmAperturaCaja ac = new frmAperturaCaja();
+            ac.Show();
+        }
+
+        private void btnCierreCaja_Click(object sender, EventArgs e)
+        {
+            frmCierreCaja cc = new frmCierreCaja();
+            cc.Show();
+        }
+
+        private void btnPagos_Click(object sender, EventArgs e)
+        {
+            frmPagosCaja pc = new frmPagosCaja();
+            pc.Show();
+        }
+
+        private void btnSalidasCaja_Click(object sender, EventArgs e)
+        {
+            frmSalidasCaja sc = new frmSalidasCaja();
+            sc.Show();
+        }
+
+        private void btnCancelarCuenta_Click(object sender, EventArgs e)
+        {
+            frmCancelarCuentaCaja cac = new frmCancelarCuentaCaja();
+            cac.Show();
+        }
+
+        private void btnCambioDolar_Click(object sender, EventArgs e)
+        {
+            frmCambioDolar cd = new frmCambioDolar();
+            cd.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmResumenCaja rc = new frmResumenCaja();
+            rc.Show();
+        }
+    }
+}
